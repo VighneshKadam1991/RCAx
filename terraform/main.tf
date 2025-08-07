c7f08c2ecab61a8875bb8c0bf327e359cdcc8ac5
Vighnesh <kadamvighnesh1991@gmail.com>
2025-08-07 15:41:36 +0100

    First commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,16 +2,4 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_secretsmanager_secret" "gpt_key" {
-  name = "rca-bot/gpt-api-key"
-}
 
-resource "aws_secretsmanager_secret_version" "gpt_key_version" {
-  secret_id     = aws_secretsmanager_secret.gpt_key.id
-  secret_string = var.gpt_api_key
-}
-
-variable "gpt_api_key" {
-  type      = string
-  sensitive = true
-}
